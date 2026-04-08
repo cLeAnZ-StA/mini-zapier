e27f0381ea2d06b640f75492aa801cc80b495208
Aayush More <aayushpmore@gmail.com>
2026-04-08 22:37:51 +0530

    made changes to port

--- mini-zapier-backend/server.js
+++ mini-zapier-backend/server.js
@@ -193,4 +193,6 @@ cron.schedule('* * * * *', async () => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 SaaS Platform running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 SaaS Platform running on port ${PORT}`);
+});
